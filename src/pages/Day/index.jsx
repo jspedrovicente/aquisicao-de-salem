eec3ec8b6fbdb0c3d5dcf1a609c9c6e14d8cf40d
jspedrovicente <jspedrogarcia@gmail.com>
2023-02-21 16:37:52 -0300

    Finished visual aspect for day page

--- src/pages/Day/index.jsx
+++ src/pages/Day/index.jsx
@@ -1,9 +1,66 @@
-
+import "./day.css"
 
 const Day = () => {
     return (
-        <div>
-            Day
+        <div className="day">
+            <h3 className="page-title">
+            Dia 1
+            </h3>
+            <button className="button">Encerrar Jogo</button>
+            <div className="dayMain">
+                <div className="event-ocurrence event">
+                        <h4>
+                        Acontecimentos
+                        </h4>
+                        <div className="large-container card-border scrollable">
+                        </div>
+                </div>
+
+                <div className="event-aliveplayers event">
+                        <h4>
+                        Jogadores Vivos
+                        </h4>
+                        <div className="small-container card-border scrollable">
+                        </div>
+                </div>
+                <div className="event-hiddenocurrence event">
+                        <h4>
+                        Ações da Noite
+                        </h4>
+                        <div className="small-container card-border scrollable">
+                        </div>
+                </div>
+                <div className="event-death event">
+                        <h4>
+                        Jogadores Mortos
+                        </h4>
+                        <div className="large-container card-border scrollable">
+                        </div>
+                </div>
+                <div className="event-status event">
+                        <h4>
+                        Status
+                        </h4>
+                        <div className="small-container card-border scrollable">
+                        </div>
+                </div>
+                <div className="event-killplayer event">
+                        <h4>
+                        Matar Jogador
+                        </h4>
+                    <div className="small-container event-killplayer-inner">
+                        <h4>Nome:</h4>
+                        <select name="playerName" id="playerName">
+                            <option value="selena">SelenaGomez</option>
+                        </select>
+                        <button className="button">Matar Jogador</button>
+                        <button className="button">Começar Noite</button>
+
+                        </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
